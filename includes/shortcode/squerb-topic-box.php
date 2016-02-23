2a1556a8ce5ca89aad9e3878e350f2989ab271bd
Paweł Sułkowski <sulkowski.pawel@gmail.com>
2016-02-23 12:47:52 +0100

    [#2598] - Add additional attributes to `squerb_topic_box`
    Attributes:
    - squerb_text
    - no_topic_page
    - show_topic_page
    - show_participants
    - show_opinions

--- includes/shortcode/squerb-topic-box.php
+++ includes/shortcode/squerb-topic-box.php
@@ -4,16 +4,33 @@ class SquerbWidgetsShortcodeSquerbTopicBox {
 
   public function squerb_topic_box($atts) {
     $atts = shortcode_atts( array(
-      'topic_id' => ''
+      'topic_id' => '',
+      'squerb_text' => '',
+      'no_topic_page' => '',
+      'show_topic_page' => '',
+      'show_participants' => '',
+      'show_opinions' => ''
       ), $atts );
 
-    return $this->squerbTopicBoxHTMLSnippet($atts['topic_id']);
+    return $this->squerbTopicBoxHTMLSnippet(
+      $atts['topic_id'],
+      $atts['squerb_text'],
+      $atts['no_topic_page'],
+      $atts['show_topic_page'],
+      $atts['show_participants'],
+      $atts['show_opinions']
+    );
   }
 
-  function squerbTopicBoxHTMLSnippet($topic_id) {
+  function squerbTopicBoxHTMLSnippet($topic_id, $squerb_text, $no_topic_page, $show_topic_page, $show_participants, $show_opinions) {
     add_action('wp_footer', array($this, 'squerbTopicBoxJSHook'));
 
     return "<div data-topic-box='{$topic_id}'"
+      . ($squerb_text ? " data-squerb-text='{$squerb_text}'" : '')
+      . ($no_topic_page ? " data-no-topic-page='{$no_topic_page}'" : '')
+      . ($show_topic_page ? " data-show-participants='{$show_topic_page}'" : '')
+      . ($show_participants ? " data-show-participants='{$show_participants}'" : '')
+      . ($show_opinions ? " data-show-opinions='{$show_opinions}'" : '')
       . '></div>';
   }
 
